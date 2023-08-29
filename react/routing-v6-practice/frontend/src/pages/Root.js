fa0f995b4782eb8a8b90a4ae3b4221607f88eb4a
kimewnseong <rladbstjd96@naver.com>
2023-08-29 16:10:48 +0900

    use react-router ^6.4
    RouterProvider, createBrowserRouter, loader, action 사용

---
+++ react/routing-v6-practice/frontend/src/pages/Root.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+import { getTokenDuration } from "../util/auth";
+
+import MainNavigation from "../components/MainNavigation";
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log("duration", tokenDuration);
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
